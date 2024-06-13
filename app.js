8ebab8c6d159e091b3fe672b2184ae101c4df159
Varalakshmmii <kirtishgokulnath@gmail.com>
2024-06-13 16:06:23 +0530

    mongodb

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const dbManager = require('./util/database');
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -24,33 +24,14 @@ app.use((req, res, next) => {
   //     next();
   //   })
   //   .catch(err => console.log(err));
+  next();
 });
 
-// app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
-
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-
-(async () => {
-    try {
-      const db = await dbManager.connect('mongodb://localhost:27017/', 'my_database');
-      const collection = db.collection('my_collection');
-        app.get('/', async (req, res) => {
-        try {
-        const result = await collection.findOne({});
-        res.send(result);
-        } catch (error) {
-          res.status(500).send('Error fetching data from MongoDB');
-        }
-      });
-  
-      app.listen(3000, () => {
-        console.log('Server is running on port 3000');
-      });
-    } catch (error) {
-      console.error("An error occurred:", error);
-    }
-  })();
-  
+mongoConnect(() => {
+  app.listen(3000);
+});
